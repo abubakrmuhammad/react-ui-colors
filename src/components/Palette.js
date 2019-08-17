20f30f3f25c111532b6086e85e67945270e5eaa6
Abubakr <realchampiontheno1@gmail.com>
2019-08-17 19:07:51 +0500

    Styled Shade Slider

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import '../style/slider.css';
 
 class Palette extends Component {
   constructor(props) {
@@ -26,14 +27,16 @@ class Palette extends Component {
 
     return (
       <div className='Pallete'>
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          // onAfterChange={this.changeLevel}
-          onChange={this.changeLevel}
-        />
+        <div className='slider'>
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            // onAfterChange={this.changeLevel}
+            onChange={this.changeLevel}
+          />
+        </div>
         <div className='Pallete__colors'>{colorBoxes}</div>
       </div>
     );
