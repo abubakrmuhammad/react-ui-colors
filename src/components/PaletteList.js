2cc8aca3ed622f1b2b10601d5bc84aed5116829b
Abubakr <realchampiontheno1@gmail.com>
2019-08-22 11:26:21 +0500

    Added Delete Dialog

--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import MiniPalette from './MiniPalette';
 import bg from '../style/bg.svg';
+import DeleteDialog from './DeleteDialog';
 
 const styles = {
   root: {
@@ -39,24 +40,63 @@ const styles = {
   }
 };
 
-function PaletteList({ classes, palettes, deletePalette }) {
-  return (
-    <div className={classes.root}>
-      <div className={classes.container}>
-        <nav className={classes.nav}>
-          <h1>React UI Colors</h1>
-          <Link to='/palette/new'>Create Palette</Link>
-        </nav>
-        <TransitionGroup className={classes.palettes}>
-          {palettes.map(palette => (
-            <CSSTransition key={palette.id} classNames='popout' timeout={500}>
-              <MiniPalette deletePalette={deletePalette} {...palette} />
-            </CSSTransition>
-          ))}
-        </TransitionGroup>
+class PaletteList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dialogOpen: false,
+      deletingPaletteId: null
+    };
+
+    this.openDialog = this.openDialog.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+    this.deletePalette = this.deletePalette.bind(this);
+  }
+
+  openDialog(event, id) {
+    event.stopPropagation();
+
+    this.setState({ dialogOpen: true, deletingPaletteId: id });
+  }
+
+  closeDialog() {
+    this.setState({ dialogOpen: false, deletingPaletteId: null });
+  }
+
+  deletePalette() {
+    this.closeDialog();
+
+    this.props.deletePalette(this.state.deletingPaletteId);
+  }
+
+  render() {
+    const { classes, palettes } = this.props;
+    const { dialogOpen } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <nav className={classes.nav}>
+            <h1>React UI Colors</h1>
+            <Link to='/palette/new'>Create Palette</Link>
+          </nav>
+          <TransitionGroup className={classes.palettes}>
+            {palettes.map(palette => (
+              <CSSTransition key={palette.id} classNames='popout' timeout={800}>
+                <MiniPalette openDialog={this.openDialog} {...palette} />
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </div>
+        <DeleteDialog
+          open={dialogOpen}
+          handleClose={this.closeDialog}
+          deletePalette={this.deletePalette}
+        />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default withStyles(styles)(PaletteList);
